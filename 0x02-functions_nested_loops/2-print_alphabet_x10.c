d6edb841535d4e4b21b6ebc42bad935c92da6b62
Cyprain <abasiodiongotung@gmail.com>
2023-03-22 16:43:47 -0700

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,21 +6,18 @@
  */
 
 void print_alphabet_x10(void)
+
 {
 	char c;
-	int i;
+	int i = 0;
 
-	i = 0;
-
-	while (i < 10)
+	while (i <= 0)
 	{
-		c = 'a';
-		while (c <= 'z')
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-			c++;
 		}
-		_putchar(c);
+		_putchar('\n');
 		i++;
 	}
 }
