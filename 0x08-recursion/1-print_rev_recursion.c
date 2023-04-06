97d9459ab86c55f7c56922f42c1e8568ba78c42c
Cyprain <abasiodiongotung@gmail.com>
2023-04-06 06:36:15 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,15 +5,13 @@
  * @s: string
  * Return: no return.
  */
+
 void _print_rev_recursion(char *s)
 
 {
-	int *s = 0
-	
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	return;
 }
